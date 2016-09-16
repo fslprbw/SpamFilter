94e85142e382d3924acc44f1b4580dc4ec56161b
mhartopo <udaraapi@rocketmail.com>
2016-09-16 15:47:23 +0700

    add stopwords list

--- src/tfidf/TfIdfTest.java
+++ src/tfidf/TfIdfTest.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import IndonesianNLP.IndonesianSentenceTokenizer;
+
 public class TfIdfTest {
 	public static void main(String[] args) {
-		String document1 = "one two one one one one one";
+		String document1 = "Halo nama saya cie haha";
 	    String document2 = "three four one oneone one";
 	    List<String> list = new ArrayList<String>();
 	    list.add(document1);
-	    //list.add(document2);
+	    list.add(document2);
 	    TfIdf tfIdf = new TfIdf(list);
 	    Map<String, Double> m = tfIdf.tfidf();
 	    System.out.println(m.get("two"));
+	    IndonesianSentenceTokenizer it = new IndonesianSentenceTokenizer();
+	    ArrayList<String> a = it.tokenizeSentence(document1);
+	    for (int i = 0; i < a.size(); i++) {
+	    	System.out.println(a.get(i));
+	    }
 	}
 }
