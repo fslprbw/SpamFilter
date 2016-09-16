c4f180bf4e35ca0a20fe7a79e46175efb05dcc01
mhartopo <udaraapi@rocketmail.com>
2016-09-16 16:57:03 +0700

    add weka library

---
+++ src/model/ImportantWords.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import IndonesianNLP.IndonesianSentenceTokenizer;
+import tfidf.TfIdf;
+
+public class ImportantWords {
+	private double treshold;
+	public ImportantWords() {
+		treshold = 5.0;
+	}
+	
+	public ImportantWords(int treshold) {
+		this.setTreshold(treshold);
+	}
+	
+	public List<String> getUniqueWords(List<String> words) {
+		List<String> result = new ArrayList<String>();
+		Set<String> uniqueGas = new HashSet<String>(words);
+		result.addAll(uniqueGas);
+		return result;
+	}
+	
+	public List<String> getUniqueWordsDoc(List<String> documents) {
+		List<String> accWords = new ArrayList<String>();
+		IndonesianSentenceTokenizer it = new IndonesianSentenceTokenizer();
+		for(String sentence : documents) {
+			ArrayList<String> words = it.tokenizeSentence(sentence);
+			accWords.addAll(words);
+		}
+		return getUniqueWords(accWords);
+	}
+	
+	public ArrayList<String> getImportantWord(List<String> documents) {
+		ArrayList<String> result = new ArrayList<String>();
+		List<String> uniqueWords = getUniqueWordsDoc(documents);
+		TfIdf tfIdf = new TfIdf(documents);
+	    Map<String, Double> m = tfIdf.tfidf();
+		for(String word : uniqueWords) {
+			if (m.get(word) >= treshold) {
+				result.add(word);
+			}
+		}
+		return result;
+	}
+	
+	
+	//Getter and setter
+	public double getTreshold() {
+		return treshold;
+	}
+
+	public void setTreshold(double treshold) {
+		this.treshold = treshold;
+	}
+}
--- src/tfidf/TfIdfTest.java
+++ src/tfidf/TfIdfTest.java
@@ -1,25 +1,30 @@
 package tfidf;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import IndonesianNLP.IndonesianSentenceTokenizer;
 
 public class TfIdfTest {
 	public static void main(String[] args) {
 		String document1 = "Halo nama saya cie haha";
-	    String document2 = "three four one oneone one";
+	    String document2 = "three four one one one one";
 	    List<String> list = new ArrayList<String>();
 	    list.add(document1);
 	    list.add(document2);
 	    TfIdf tfIdf = new TfIdf(list);
 	    Map<String, Double> m = tfIdf.tfidf();
-	    System.out.println(m.get("two"));
 	    IndonesianSentenceTokenizer it = new IndonesianSentenceTokenizer();
-	    ArrayList<String> a = it.tokenizeSentence(document1);
-	    for (int i = 0; i < a.size(); i++) {
-	    	System.out.println(a.get(i));
+	    ArrayList<String> a = it.tokenizeSentence(document2);
+	    List<String> list1 = a;
+	    Set<String> uniqueGas = new HashSet<String>(list1);
+	    System.out.println("Unique gas count: " + uniqueGas.size());
+	    Object[] c = uniqueGas.toArray();
+	    for(int i = 0; i < c.length; i++) {
+	    	System.out.println(c[i]);
 	    }
 	}
 }
